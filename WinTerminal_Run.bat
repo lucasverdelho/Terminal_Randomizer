58980fef32b7ea2c793e45d36da89d320d6146bd
LucasVerdelho <a93247@alunos.uminho.pt>
2022-10-24 18:10:56 +0100

    fix nos paths

--- WinTerminal_Run.bat
+++ WinTerminal_Run.bat
@@ -1,6 +1,7 @@
 @echo off
-cd "C:\Users\lv\Desktop\Terminal_Randomizer"
+cd "C:\Users\lv\Desktop\Terminal_Randomizer\src"
 
 python terminal_randomizer.py
 
 "C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.15.2874.0_x64__8wekyb3d8bbwe\wt.exe"
+pause
