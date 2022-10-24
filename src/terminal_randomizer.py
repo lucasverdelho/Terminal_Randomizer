9da9ddd494c79b194ace6ede2ab62b4391569700
LucasVerdelho <a93247@alunos.uminho.pt>
2022-10-24 23:46:06 +0100

    Working But Not Good Looking

--- src/terminal_randomizer.py
+++ src/terminal_randomizer.py
@@ -23,11 +23,26 @@ photos_path = '..\\photos'
 photos = os.listdir(photos_path)
 random_bg = random.choice(photos)
 
+# Choose color file from folder
+colors_path = '..\\thumbnails'
+colors_path = os.path.join(colors_path, (random_bg[:-4] + ".txt"))
+
+# Load colors from file
+with open(colors_path, 'r') as f:
+    colors = f.readlines()
+    f.close()
+
+# Select Random Color From Given Set
+random_color = random.choice(colors)
+
+
 
 # Write all parameters to be change to the settings.json file
 with open(json_settings_path, 'w') as settings_file:
     
     settings['profiles']['list'][1]['backgroundImage'] = os.path.join(os.path.abspath(photos_path), random_bg)
+    settings['profiles']['list'][1]['cursorColor'] = colors[3].replace('\n', '')
+    settings['profiles']['list'][1]['selectionBackground'] = colors[1].replace('\n', '')
     json.dump(settings, settings_file, indent=4)
     
 
