3f826dfb505f21891bbd9f6abb8449e17018fac6
LucasVerdelho <a93247@alunos.uminho.pt>
2022-10-24 18:08:13 +0100

    Fix do Batch file

---
+++ WinTerminal_Run.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\lv\Desktop\Terminal_Randomizer"
+
+python terminal_randomizer.py
+
+"C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.15.2874.0_x64__8wekyb3d8bbwe\wt.exe"
