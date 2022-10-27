e8f0a88a3f89bce54268e83540aaa0d07327f7b5
LucasVerdelho <a93247@alunos.uminho.pt>
2022-10-27 20:39:47 +0100

    Satisfactory Color Generation, maybe some kinks to iron out

--- src/pre_load_images.py
+++ src/pre_load_images.py
@@ -12,8 +12,20 @@ def rgb_to_hex(rgb_color):
     return hex_color
 
 
+# Brighten a color
+def color_variant(hex_color, brightness_offset=1):
+
+    if len(hex_color) != 7:
+        raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
+
+    rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
+    new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
+    new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255
+    # hex() produces "0x88", we want just "88"
+    return "#" + "".join([hex(i)[2:] for i in new_rgb_int])
+
+
 # Function that returns the complimentary of a given color
-# TODO - Still not working great, need to brighten the output color as it is unreadable
 def get_complementary(color):
     color = color[1:]
     color = int(color, 16)
@@ -48,7 +60,6 @@ thumbnails_path = "..\\thumbnails\\"
 
 
 # To get the colors from the thumbnails
-# TODO - Improve the color selection
 for i in thumbnails:
     if (i[-4:] == ".txt"):
         os.remove(os.path.join(thumbnails_path, i))
@@ -62,10 +73,20 @@ for i in thumbnails:
     filename = thumbnails_path + i[1:-4] + ".txt"
     filename = filename.replace("converted", "")
 
+    def is_dark_color(color):
+        return sum(color) < 382
+
     try:
         with open(filename, "w") as f:
-            f.write(get_complementary(rgb_to_hex(palette[3])) + "\n")
+            
+            temporary_complimentary = color_variant(get_complementary(rgb_to_hex(palette[0])),80)
+            if (is_dark_color(palette[0])):
+                temporary_complimentary = color_variant(rgb_to_hex(palette[0]), 120)
+            
+            f.write(temporary_complimentary + "\n")
             for i in palette:
+                # if(i == palette[0]):
+                #     f.write(color_variant(rgb_to_hex(i), 50))
                 f.write(rgb_to_hex(i) + "\n")
     except Exception as e:
         print(e)
